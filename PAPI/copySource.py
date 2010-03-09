83e2e72a9b4be3372afd7c4fbc1fcb71112b03ef
panic <panic>
2010-03-09 17:27:38 +0000

    Convert to function mode

--- PAPI/copySource.py
+++ PAPI/copySource.py
@@ -8,21 +8,31 @@ import glob
 __author__="panic"
 __date__ ="$Apr 27, 2009 06:00:23 PM$"
 
+
+def linkSourceFiles( source ):
+"""Create a symbolic link to all the sources specified in 'source', which can be a file a dir"""
+  
+    if (os.path.isfile(source)):
+        #We have a source-file with absolute path for the data files
+        file=open(source, 'r')
+        for line in file:
+            print "FILE=", line
+            if line.endswith('\n'):
+                line=line.replace('\n','')
+            os.symlink(line, os.path.basename(line))
+    else:
+        #We have a source-directory as input data
+        for file in glob.glob(source+"*.fits"):
+            print "FILE=", file
+            os.symlink(file, os.path.basename(file))
+
+#################################################################
+
 if __name__ == "__main__":
+    
     if len(sys.argv)>1:
         source = sys.argv[1]
-        if (os.path.isfile(source)):
-           #We have a source-file with absolute path for the data files
-           file=open(source, 'r')
-           for line in file:
-                print "FILE=", line
-                if line.endswith('\n'):
-                  line=line.replace('\n','')
-                os.symlink(line, os.path.basename(line))
-        else:
-           #We have a source-directory as input data
-            for file in glob.glob(source+"*.fits"):
-                print "FILE=", file
-                os.symlink(file, os.path.basename(file))
+        linkSourceFiles(source)
     else:
-        sys.exit(0)
+        print "Wrong number of arguments"
+        sys.exit(0)      
