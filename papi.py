219ad6c1986b57f945ca092db4a584d635dd6e8a
jmiguel <jmiguel@iaa.es>
2011-11-09 08:13:32 +0000

    A test

--- papi.py
+++ papi.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Copyright (c) 2009 Jose M. Ibanez. All rights reserved.
+# Copyright (c) 2009-2011 Jose M. Ibanez. All rights reserved.
 # Institute of Astrophysics of Andalusia, IAA-CSIC
 #
 # This file is part of PAPI (PANIC Pipeline)
