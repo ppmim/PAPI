a3bf6a9512c45a1a228600ef04efa9c3b62f750b
integrador <integrador>
2013-07-17 07:55:30 +0000

    Added QT_GRAPHICSSYSTEM to compile.sh script.

--- QL4/compile.sh
+++ QL4/compile.sh
@@ -3,11 +3,13 @@
 
 #/home/panic/SOFTWARE/PAPI/PyQt-x11-gpl-4.9.4/pyuic/pyuic4 panicQL.ui > panicQL.py
 
+export QT_GRAPHICSSYSTEM=native
+
 pyuic4 panicQL.ui > panicQL.py
 
 if [[ `hostname -s` = udit22* ]]; then
 	python runQL.py  -c ../config_files/papi.cfg
 else
 	#python runQL.py  -c ../config_files/papi_irws2.cfg
-	python runQL.py  -c ../config_files/papi_portatil.cfg
+	python runQL.py  -c ../config_files/papi_panic2.cfg
 fi
