8aba9e21a89b1a696dd52f10b848a163deb715fd
jmiguel <jmiguel@iaa.es>
2013-09-25 18:57:35 +0200

    Update to match master and limpia branches

--- papi_setup.sh
+++ papi_setup.sh
@@ -2,7 +2,7 @@
 # User Configurable Settings
 #------------------------------------------------------------------------------
 # path to PAPI directory
-export PAPI_HOME=${HOME}/pipelines/PAPI
+export PAPI_HOME=${HOME}/papi
 
 # path to PAPI output data products
 export PAPI_PROD=${HOME}/DataProd
@@ -11,6 +11,19 @@ export PAPI_PROD=${HOME}/DataProd
 # Fixed Settings
 #------------------------------------------------------------------------------
 # path to PAPI reference files
-export PAPI_CONFIG=${PAPI_HOME}/config_files
+export PAPI_CONFIG=${PAPI_HOME}/config_files/papi.cfg
 export PATH=${PATH}:${PAPI_HOME}/bin
-export PYTHONPATH=${PYTHONPATH}:${PAPI_HOME}/lib
+export PYTHONPATH=${PYTHONPATH}:${PAPI_HOME}
+
+
+#--------------------------------
+# IRAF settings
+#--------------------------------
+mkiraf 
+
+#-------------------------------
+# IRDR build
+#------------------------------
+cd irdr
+make all
+
