10dd3b08f3fcf972b4c856d50bdaf9bce0bfb435
JoseMiguel <jmiguel@iaa.e>
2015-02-24 08:36:06 +0100

    Added new tool to compute the dither offset of a set of images based on the WCS of the header.

---
+++ commissioning/getImageOffsets.py
@@ -0,0 +1,174 @@
+#! /usr/bin/env python
+#encoding:UTF-8
+
+# Copyright (c) 2015 Jose M. Ibanez All rights reserved.
+# Institute of Astrophysics of Andalusia, IAA-CSIC
+#
+# This file is part of PAPI
+#
+# PAPI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import math
+import sys
+import os
+import logging as log
+import fileinput
+from optparse import OptionParser
+
+
+import numpy
+import astropy.io.fits as fits
+from astropy import wcs
+
+
+
+def getWCSPointingOffsets(images_in,
+                              p_offsets_file="/tmp/offsets.txt"):
+      """
+      Derive pointing offsets of each image taking as reference the first one and
+      using WCS astrometric calibration of the images. Note that is **very** 
+      important that the images are astrometrically calibrated.
+      
+        Parameters
+        ----------
+                
+        images_in: str
+            list of filename of images astrometrically calibrated
+        p_offsets_file: str
+            filename of file where offset will be saved (it can be used
+            later by dithercubemean). The format of the file will be:
+            
+            /path/to/filename00   offsetX00   offsetY00
+            /path/to/filename01   offsetX01   offsetY01
+            ...
+            /path/to/filename0N   offsetX0N   offsetY0N
+            
+        Returns
+        -------    
+        offsets: narray          
+                two dimensional array (Nx2) with offsets (in pixles),
+                where N=number of files.
+                
+        Notes:
+            It assumed that the input images have a good enough astrometric
+            calibration (hopefully obtained with Astrometry.net), and North
+            is up and East is left, and no rotation angle exists.
+            
+        
+      """
+      
+      log.info("Starting getWCSPointingOffsets....")
+
+      
+      # Very important the pixel scale in order to find out good offsets values !!
+      pix_scale = 0.45
+      # Init variables
+      i = 0 
+      offsets_mat = None
+      ra0 = -1
+      dec0 = -1
+      offsets = numpy.zeros([len(images_in), 2] , dtype=numpy.float32)
+      
+      # Reference image
+      ref_image = images_in[0]
+      try:
+            h0 = fits.getheader(ref_image)
+            # We use the center of the image as reference to get the offsets
+            x_pix = h0['NAXIS1']/2.0
+            y_pix = h0['NAXIS2']/2.0
+            # If present, pix_scale in header is prefered
+            if 'PIXSCALE' in h0: pix_scale = h0['PIXSCALE']
+            w0 = wcs.WCS(h0)
+            ra0 = w0.wcs_pix2world(x_pix, y_pix, 1)[0]
+            dec0 = w0.wcs_pix2world(x_pix, y_pix, 1)[1]
+            print "RA",ra0
+            print "DEC",dec0
+            log.debug("Ref. image: %s RA0= %s DEC0= %s PIXSCALE= %f"%(ref_image, ra0, dec0, pix_scale))
+      except Exception,e:
+          raise e
+        
+      offset_txt_file = open(p_offsets_file, "w")
+      for my_image in images_in:
+        try:
+              h = fits.getheader(my_image)
+              w = wcs.WCS(h)
+              ra = w.wcs_pix2world(x_pix, y_pix, 1)[0]
+              dec = w.wcs_pix2world(x_pix, y_pix, 1)[1]
+              log.debug("Image: %s RA[%d]= %s DEC[%d]= %s"%(my_image, i,ra, i, dec))
+              pix_scale = 1.0 # to gives the offsets in arcsec scale
+              # Assummed that North is up and East is left
+              offsets[i][0] = ((ra - ra0)*3600*math.cos(dec0/57.296)) / float(pix_scale)
+              offsets[i][1] = ((dec0 - dec)*3600) / float(pix_scale)
+              
+              log.debug("offset_ra  = %s"%offsets[i][0])
+              log.debug("offset_dec = %s"%offsets[i][1])
+              
+              offset_txt_file.write(my_image + "   " + "%.6f   %0.6f\n"%(offsets[i][0], offsets[i][1]))
+              i+=1
+        except Exception,e:
+          raise e
+        
+      offset_txt_file.close()
+      
+      # Write out offsets to file
+      # numpy.savetxt(p_offsets_file, offsets, fmt='%.6f')
+      log.debug("(WCS) Image Offsets (pixels): ")
+      log.debug(offsets)
+      
+      return offsets
+  
+
+#################
+# MAIN #
+#################
+if __name__ == "__main__":
+    
+    usage = "usage: %prog [options]"
+    desc = """Gives the image offsets (in based on the WCS of the image headers."""
+    
+    parser = OptionParser(usage, description=desc)
+    
+    parser.add_option("-s", "--source", type="str",
+                  action="store", dest="source_file",
+                  help="Input text file with the list of FITS file to be analized.")
+                  
+    parser.add_option("-o", "--output", type="str",
+                  action="store", dest="output_filename", 
+                  help="Output file to write the offset matrix")
+    
+    (options, args) = parser.parse_args()
+    
+    if len(sys.argv[1:]) < 1:
+       parser.print_help()
+       sys.exit(0)
+        
+    # args is the leftover positional arguments after all options have been processed 
+    if not options.source_file or not options.output_filename or len(args)!=0: 
+        parser.print_help()
+        parser.error("incorrect number of arguments " )
+        
+    
+    if os.path.isfile(options.source_file):
+        files = [line.replace("\n", "").replace('//','/')
+            for line in fileinput.input(options.source_file)]
+    else:
+        print "Error, cannot read file : ", options.source_file
+        sys.exit(0)
+        
+    offsets = getWCSPointingOffsets(files, options.output_filename)
+    
+    numpy.set_printoptions(suppress=True)
+    print "\nOffset Matrix: \n"
+    print(offsets)
